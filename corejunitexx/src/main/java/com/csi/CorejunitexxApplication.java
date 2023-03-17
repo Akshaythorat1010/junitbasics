e3af838bc58a87c78020eb700150d9cc67f50911
Akshaythorat1010 <akshaythorat004@gmail.com>
2023-03-17 15:00:19 +0530

    [STNO-901] Initial commit for junit test example

---
+++ corejunitexx/src/main/java/com/csi/CorejunitexxApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorejunitexxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorejunitexxApplication.class, args);
+	}
+
+}
